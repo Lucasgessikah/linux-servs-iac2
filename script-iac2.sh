157dc977059facff08862bde70e911beca129df9
Lucasgessikah <lucas_jose_araujo@hotmail.com>
2022-08-11 00:42:50 +0000

    Corrigindo erro

--- script-iac2.sh
+++ script-iac2.sh
@@ -11,5 +11,5 @@ echo "Baixando e copiando arquivos..."
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
-cd linux-site-dio
+cd linux-site-dio-main
 cp -R * /var/www/html/
